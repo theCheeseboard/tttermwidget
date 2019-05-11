1dc0584cd582fc985962eb7703706aa30870731d
Victor Tran <vicr12345@gmail.com>
2019-05-11 23:47:37 +1000

    Add translations

---
+++ translations/lg_UG.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ug_CN" sourcelanguage="en_US">
+<context>
+    <name>ColorScheme</name>
+    <message>
+        <location filename="../lib/ColorScheme.cpp" line="278"/>
+        <source>Unnamed Color Scheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Konsole::TerminalDisplay</name>
+    <message>
+        <location filename="../lib/TerminalDisplay.cpp" line="3150"/>
+        <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Konsole::Vt102Emulation</name>
+    <message>
+        <location filename="../lib/Vt102Emulation.cpp" line="1096"/>
+        <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../lib/ColorScheme.cpp" line="292"/>
+        <source>Un-named Color Scheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/Filter.cpp" line="518"/>
+        <source>Open Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/Filter.cpp" line="519"/>
+        <source>Copy Link Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/Filter.cpp" line="523"/>
+        <source>Send Email To...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/Filter.cpp" line="524"/>
+        <source>Copy Email Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchBar</name>
+    <message>
+        <location filename="../lib/SearchBar.ui" line="78"/>
+        <source>Find</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/SearchBar.cpp" line="56"/>
+        <source>Match case</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/SearchBar.cpp" line="62"/>
+        <source>Regular expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/SearchBar.cpp" line="66"/>
+        <source>Highlight all matches</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TTTermWidget</name>
+    <message>
+        <location filename="../lib/tttermwidget.cpp" line="476"/>
+        <source>Color Scheme Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lib/tttermwidget.cpp" line="477"/>
+        <source>Cannot load color scheme: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
