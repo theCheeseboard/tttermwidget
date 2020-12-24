dd4ee2b9b0117077bb0e6fb31ff6628fe910757e
Atahan Reis <atahanreis@icloud.com>
2020-12-24 16:38:49 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: theTerminal/theTerminal Core
    Translate-URL: https://translate.vicr123.com/projects/theterminal/theterminal-core/en_GB/

--- translations/en_GB.ts
+++ translations/en_GB.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>ColorScheme</name>
     <message>
         <location filename="../lib/ColorScheme.cpp" line="278"/>
         <source>Unnamed Color Scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Unnamed Color Scheme</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../lib/TerminalDisplay.cpp" line="3164"/>
         <source>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;qt&gt;Output has been &lt;a href=&quot;http://en.wikipedia.org/wiki/Flow_control&quot;&gt;suspended&lt;/a&gt; by pressing Ctrl+S.  Press &lt;b&gt;Ctrl+Q&lt;/b&gt; to resume.&lt;/qt&gt;</translation>
     </message>
 </context>
 <context>
@@ -22,7 +22,7 @@
     <message>
         <location filename="../lib/Vt102Emulation.cpp" line="1102"/>
         <source>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</source>
-        <translation type="unfinished"></translation>
+        <translation>No keyboard translator available.  The information needed to convert key presses into characters to send to the terminal is missing.</translation>
     </message>
 </context>
 <context>
@@ -30,27 +30,27 @@
     <message>
         <location filename="../lib/ColorScheme.cpp" line="292"/>
         <source>Un-named Color Scheme</source>
-        <translation type="unfinished"></translation>
+        <translation>Un-named Color Scheme</translation>
     </message>
     <message>
         <location filename="../lib/Filter.cpp" line="518"/>
         <source>Open Link</source>
-        <translation type="unfinished"></translation>
+        <translation>Open Link</translation>
     </message>
     <message>
         <location filename="../lib/Filter.cpp" line="519"/>
         <source>Copy Link Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy Link Address</translation>
     </message>
     <message>
         <location filename="../lib/Filter.cpp" line="523"/>
         <source>Send Email To...</source>
-        <translation type="unfinished"></translation>
+        <translation>Send Email To...</translation>
     </message>
     <message>
         <location filename="../lib/Filter.cpp" line="524"/>
         <source>Copy Email Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy Email Address</translation>
     </message>
 </context>
 <context>
@@ -58,22 +58,22 @@
     <message>
         <location filename="../lib/SearchBar.ui" line="78"/>
         <source>Find</source>
-        <translation type="unfinished"></translation>
+        <translation>Find</translation>
     </message>
     <message>
         <location filename="../lib/SearchBar.cpp" line="56"/>
         <source>Match case</source>
-        <translation type="unfinished"></translation>
+        <translation>Match case</translation>
     </message>
     <message>
         <location filename="../lib/SearchBar.cpp" line="62"/>
         <source>Regular expression</source>
-        <translation type="unfinished"></translation>
+        <translation>Regular expression</translation>
     </message>
     <message>
         <location filename="../lib/SearchBar.cpp" line="66"/>
         <source>Highlight all matches</source>
-        <translation type="unfinished"></translation>
+        <translation>Highlight all matches</translation>
     </message>
 </context>
 <context>
@@ -81,12 +81,12 @@
     <message>
         <location filename="../lib/tttermwidget.cpp" line="425"/>
         <source>Color Scheme Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Color Scheme Error</translation>
     </message>
     <message>
         <location filename="../lib/tttermwidget.cpp" line="426"/>
         <source>Cannot load color scheme: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cannot load color scheme: %1</translation>
     </message>
 </context>
 </TS>
